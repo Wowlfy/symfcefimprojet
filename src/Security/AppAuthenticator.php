4434d9e14580455cfcbd388cefe10b1e3337f842
Dorine <giustinadorine@gmail.com>
2022-06-08 16:01:25 +0200

    fix security.yaml

--- src/Security/AppAuthenticator.php
+++ src/Security/AppAuthenticator.php
@@ -31,7 +31,7 @@ class AppAuthenticator extends AbstractLoginFormAuthenticator
     public function authenticate(Request $request): Passport
     {
         $email = $request->request->get('email', '');
-
+  
         $request->getSession()->set(Security::LAST_USERNAME, $email);
 
         return new Passport(
