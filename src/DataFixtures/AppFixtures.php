3c94a2e9688d76338c242bbe5daf3c287baa9172
Dorine <giustinadorine@gmail.com>
2022-06-08 12:19:31 +0200

    feat: SkilFix ok/UFix ok/SkilCatFix ok/ProDet->bug

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,42 +1,16 @@
 <?php
 
 namespace App\DataFixtures;
-
-use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
-    private UserPasswordHasherInterface $passwordHasher;
-    public function __construct(UserPasswordHasherInterface $passwordHasher)
-    {
-        $this->passwordHasher = $passwordHasher;
-    }
+
     public function load(ObjectManager $manager): void
     {
-        // $faker = Factory::create('fr_FR');
-        // //user
-        // for($u = 0, $uMax =10; $u<$uMax; $u++){
-        //     $user = new User();
-        //     $user
-        //         ->setFirstname($faker->firstName)
-        //         ->setLastname($faker->lastName)
-        //         ->setEmail($faker->email)
-        //         ->setPassword($this->passwordHasher->hashPassword($user, '3366'));
-        //         $manager->persist($user);
-        // }
-        //  //admin
-        //  $admin = new User();
-        //  $admin
-        //      ->setFirstname('dorine')
-        //      ->setLastname('giustina')
-        //      ->setEmail('dodox@gmail.com')
-        //      ->setRoles(['ROLE_ADMIN'])
-        //      ->setPassword($this->passwordHasher->hashPassword($user, '3366'));
-        //      $manager->persist($admin);
-        //  $manager->flush();
+        $manager->flush();
+        //run symfony console d:f:l
     }
 }
+
